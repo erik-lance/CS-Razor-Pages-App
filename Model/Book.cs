ab339c57feca0b595538e468e96ca4534e5161ca
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2023-07-03 12:40:34 +0800

    Added initial book details

---
+++ Model/Book.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_Razor_Pages_App.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        
+    }
+}
